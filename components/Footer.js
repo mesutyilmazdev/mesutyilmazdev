7dcb00209087a3fc02db7eec15171caa5d1299c7
Mesut Yılmaz <42286402+mesutyilmazdev@users.noreply.github.com>
2023-11-25 19:44:27 +0300

    Update Footer.js
    threads linki kaldırıldı

--- components/Footer.js
+++ components/Footer.js
@@ -19,12 +19,6 @@ const Footer = () => {
           </Tippy>
         </Link>
 
-        <Link href="https://www.threads.net/@mesutyilmazdev" passHref>
-          <Tippy content="Threads">
-            <a href="https://www.threads.net/@mesutyilmazdev" rel="noreferrer" target="_blank"><i className='bi bi-threads text-gray-300 border border-gray-300 hover:bg-gray-300 hover:text-[#273767] focus:ring-4 rounded-full p-3 inline-flex items-center m-2 cursor-pointer ease-in-out duration-300' /></a>
-          </Tippy>
-        </Link>
-
         <Link href="https://www.youtube.com/channel/UCatbgDblHcN_X8gUDaImvcw" passHref>
           <Tippy content="Youtube">
             <a href="https://www.youtube.com/channel/UCatbgDblHcN_X8gUDaImvcw" rel="noreferrer" target="_blank"><i className='bi bi-youtube text-gray-300 border border-gray-300 hover:bg-gray-300 hover:text-[#273767] focus:ring-4 rounded-full p-3 inline-flex items-center m-2 cursor-pointer ease-in-out duration-300' /></a>
