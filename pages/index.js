33f264bcc1465c58c78f26ee98b7b37821c7815b
Mesut Yılmaz <42286402+mesutyilmazdev@users.noreply.github.com>
2024-12-03 14:23:35 +0300

    Update index.js
    geri aldık ADS

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Content from '../components/Content'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Home() {
   return (
     <div className='container-lg flex flex-col mx-auto'>
-    <GoogleAnalytics gaId="G-PFNQT0LP7R" />
       <Header/>
       <main>
         <Content/>
